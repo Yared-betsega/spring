f8d671726a125d9763c4f9ad9014b9fdfdf77ed0
Yared <yaredtsegaye120@gmail.com>
2022-01-07 01:02:20 +0300

    validate forms

--- taco-cloud/src/main/java/com/yared/tacocloud/OrderController.java
+++ taco-cloud/src/main/java/com/yared/tacocloud/OrderController.java
@@ -2,10 +2,14 @@ package com.yared.tacocloud;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.slf4j.Slf4j;
+
+import javax.validation.Valid;
+
 import com.yared.tacocloud.TacoOrder;
 
 @Slf4j
@@ -19,7 +23,10 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(TacoOrder order) {
+    public String processOrder(@Valid TacoOrder order, Errors errors) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
         log.info("Order submitted: " + order);
         return "redirect:/";
     }
